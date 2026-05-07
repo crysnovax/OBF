32a22e7ee9a79468431661d83dd4473d49ffcbee
CRYSTAL-LEVI UZOUKWU <crysnovax@gmail.com>
2026-05-07 12:14:14 +0100

    🏷️

--- worker.js
+++ worker.js
@@ -1,4 +1,5 @@
 //******************obf technology by crysnovax***********************//
+
 const CORS_HEADERS = {
   'Access-Control-Allow-Origin': '*',
   'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
